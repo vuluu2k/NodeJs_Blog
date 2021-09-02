8123774c73be0227476c83de89c07a6db1894c11
Lưu Công Quang Vũ <tieungpk1@gmail.com>
2021-09-02 21:53:10 +0700

    update

--- src/index.js
+++ src/index.js
@@ -4,19 +4,23 @@ const morgan=require('morgan')
 const handlebars  = require('express-handlebars')
 const app = express()
 const port = 3000
+
+const route=require('./routes')
+
+app.use(express.static(path.join(__dirname,'public')))
+app.use(express.urlencoded(
+  {
+    extended: true
+  }
+))
+app.use(express.json())
 app.use(morgan('combined'))
 app.engine('hbs', handlebars({
   extname:'hbs'
 }));
 app.set('view engine', 'hbs');
 app.set('views',path.join(__dirname,'resources\\views'))
-app.get('/', (req, res) => {
-  res.render('home')
-})
-app.get('/news', (req, res) => {
-  res.render('news')
-})
-
+route(app)
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
